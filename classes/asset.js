c008bbc1fc82d155221cac16538ce02b2b40e89d
Jon Burk <jon@burk.com>
2018-12-16 12:43:25 -0500

    add variance to assets

--- classes/asset.js
+++ classes/asset.js
@@ -7,13 +7,15 @@ import { calculateStandardDeviation } from '../utils/stats'
 class Asset {
   constructor (symbol, history, beta, expectedReturn) {
     const meanReturn = _.meanBy(history, 'changePercent')
+    const standardDeviation = calculateStandardDeviation(history.map(quote => quote.changePercent), meanReturn)
 
     this.symbol = symbol
     this.history = history
     this.beta = beta
     this.expectedReturn = expectedReturn
     this.meanReturn = meanReturn
-    this.standardDeviation = calculateStandardDeviation(history.map(quote => quote.changePercent), meanReturn)
+    this.standardDeviation = standardDeviation
+    this.variance = standardDeviation ^ 2
   }
 }
 
